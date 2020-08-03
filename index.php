0e6bb0999a9d7ac182ba3c762fbc71db304f1790
JP Palus <mal@malekith.host>
2020-08-03 17:46:36 +0200

    clean up

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 error_reporting( error_reporting() & ~E_NOTICE );
 
 //Security options
-$allow_delete = true; // Set to false to disable delete button and delete POST request.
-$allow_upload = true; // Set to true to allow upload files
-$allow_create_folder = true; // Set to false to disable folder creation
 $allow_direct_link = true; // Set to false to only allow downloads and not direct link
 $allow_show_folders = true; // Set to false to hide all subdirectories
 
@@ -16,7 +13,6 @@ $hidden_patterns = ['*.php','.*']; // Matching files hidden in directory index
 $PASSWORD = '';  // Set the password, to access the file manager... (optional)
 
 if($PASSWORD) {
-
 	session_start();
 	if(!$_SESSION['_sfm_allowed']) {
 		// sha1, and random bytes to thwart timing attacks.  Not meant as secure hashing.
@@ -71,8 +67,6 @@ if($_GET['do'] == 'list') {
 				'name' => basename($i),
 				'path' => preg_replace('@^\./@', '', $i),
 				'is_dir' => is_dir($i),
-				'is_deleteable' => $allow_delete && ((!is_dir($i) && is_writable($directory)) ||
-				(is_dir($i) && is_writable($directory) && is_recursively_deleteable($i))),
 				'is_readable' => is_readable($i),
 				'is_writable' => is_writable($i),
 				'is_executable' => is_executable($i),
@@ -88,27 +82,7 @@ if($_GET['do'] == 'list') {
 	}
 	echo json_encode(['success' => true, 'is_writable' => is_writable($file), 'results' =>$result]);
 	exit;
-} elseif ($_POST['do'] == 'delete') {
-	if($allow_delete) {
-		rmrf($file);
-	}
-	exit;
-} elseif ($_POST['do'] == 'mkdir' && $allow_create_folder) {
-	// don't allow actions outside root. we also filter out slashes to catch args like './../outside'
-	$dir = $_POST['name'];
-	$dir = str_replace('/', '', $dir);
-	if(substr($dir, 0, 2) === '..')
-	exit;
-	chdir($file);
-	@mkdir($_POST['name']);
-	exit;
-} elseif ($_POST['do'] == 'upload' && $allow_upload) {
-	foreach($disallowed_patterns as $pattern)
-	if(fnmatch($pattern, $_FILES['file_data']['name']))
-	err(403,"Files of this type are not allowed.");
-
-	$res = move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']);
-	exit;
+	
 } elseif ($_GET['do'] == 'download') {
 	foreach($disallowed_patterns as $pattern)
 	if(fnmatch($pattern, $file))
@@ -141,31 +115,6 @@ function is_entry_ignored($entry, $allow_show_folders, $hidden_patterns) {
 	return false;
 }
 
-function rmrf($dir) {
-	if(is_dir($dir)) {
-		$files = array_diff(scandir($dir), ['.','..']);
-		foreach ($files as $file)
-		rmrf("$dir/$file");
-		rmdir($dir);
-	} else {
-		unlink($dir);
-	}
-}
-
-function is_recursively_deleteable($d) {
-	$stack = [$d];
-	while($dir = array_pop($stack)) {
-		if(!is_readable($dir) || !is_writable($dir))
-		return false;
-		$files = array_diff(scandir($dir), ['.','..']);
-		foreach($files as $file) if(is_dir($file)) {
-			$stack[] = "$dir/$file";
-		}
-	}
-	return true;
-}
-
-// from: http://php.net/manual/en/function.realpath.php#84012
 function get_absolute_path($path) {
 	$path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
 	$parts = explode(DIRECTORY_SEPARATOR, $path);
@@ -433,6 +382,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 	<script src="../resources/jquery-3.5.1.min.js"></script>
 
 	<script>
+
 	(function($){
 		$.fn.tablesorter = function() {
 			var $table = this;
@@ -443,6 +393,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			});
 			return this;
 		};
+
 		$.fn.tablesortby = function(idx,direction) {
 			var $rows = this.find('tbody tr');
 			function elementToVal(a) {
@@ -450,10 +401,12 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 				var a_val = $a_elem.attr('data-sort') || $a_elem.text();
 				return (a_val == parseInt(a_val) ? parseInt(a_val) : a_val);
 			}
+
 			$rows.sort(function(a,b){
 				var a_val = elementToVal(a), b_val = elementToVal(b);
 				return (a_val > b_val ? 1 : (a_val == b_val ? 0 : -1)) * (direction ? 1 : -1);
 			})
+
 			this.find('th').removeClass('sort_asc sort_desc');
 			$(this).find('thead th:nth-child('+(idx+1)+')').addClass(direction ? 'sort_desc' : 'sort_asc');
 			for(var i =0;i<$rows.length;i++)
@@ -461,6 +414,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			this.settablesortmarkers();
 			return this;
 		}
+
 		$.fn.retablesort = function() {
 			var $e = this.find('thead th.sort_asc, thead th.sort_desc');
 			if($e.length)
@@ -468,13 +422,16 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 
 			return this;
 		}
+
 		$.fn.settablesortmarkers = function() {
 			this.find('thead th span.indicator').remove();
 			this.find('thead th.sort_asc').append('<span class="indicator">&darr;<span>');
 			this.find('thead th.sort_desc').append('<span class="indicator">&uarr;<span>');
 			return this;
 		}
+
 	})(jQuery);
+
 	$(function(){
 		var XSRF = (document.cookie.match('(^|; )_sfm_xsrf=([^;]*)')||0)[2];
 		var MAX_UPLOAD_SIZE = <?php echo $MAX_UPLOAD_SIZE ?>;
@@ -500,73 +457,6 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			return false;
 		});
 
-		<?php if($allow_upload): ?>
-		// file upload stuff
-		$('#file_drop_target').on('dragover',function(){
-			$(this).addClass('drag_over');
-			return false;
-		}).on('dragend',function(){
-			$(this).removeClass('drag_over');
-			return false;
-		}).on('drop',function(e){
-			e.preventDefault();
-			var files = e.originalEvent.dataTransfer.files;
-			$.each(files,function(k,file) {
-				uploadFile(file);
-			});
-			$(this).removeClass('drag_over');
-		});
-		$('input[type=file]').change(function(e) {
-			e.preventDefault();
-			$.each(this.files,function(k,file) {
-				uploadFile(file);
-			});
-		});
-
-
-		function uploadFile(file) {
-			var folder = decodeURIComponent(window.location.hash.substr(1));
-
-			if(file.size > MAX_UPLOAD_SIZE) {
-				var $error_row = renderFileSizeErrorRow(file,folder);
-				$('#upload_progress').append($error_row);
-				window.setTimeout(function(){$error_row.fadeOut();},5000);
-				return false;
-			}
-
-			var $row = renderFileUploadRow(file,folder);
-			$('#upload_progress').append($row);
-			var fd = new FormData();
-			fd.append('file_data',file);
-			fd.append('file',folder);
-			fd.append('xsrf',XSRF);
-			fd.append('do','upload');
-			var xhr = new XMLHttpRequest();
-			xhr.open('POST', '?');
-			xhr.onload = function() {
-				$row.remove();
-				list();
-			};
-			xhr.upload.onprogress = function(e){
-				if(e.lengthComputable) {
-					$row.find('.progress').css('width',(e.loaded/e.total*100 | 0)+'%' );
-				}
-			};
-			xhr.send(fd);
-		}
-		function renderFileUploadRow(file,folder) {
-			return $row = $('<div/>')
-			.append( $('<span class="fileuploadname" />').text( (folder ? folder+'/':'')+file.name))
-			.append( $('<div class="progress_track"><div class="progress"></div></div>')  )
-			.append( $('<span class="size" />').text(formatFileSize(file.size)) )
-		};
-		function renderFileSizeErrorRow(file,folder) {
-			return $row = $('<div class="error" />')
-			.append( $('<span class="fileuploadname" />').text( 'Error: ' + (folder ? folder+'/':'')+file.name))
-			.append( $('<span/>').html(' file size - <b>' + formatFileSize(file.size) + '</b>'
-			+' exceeds max upload size of <b>' + formatFileSize(MAX_UPLOAD_SIZE) + '</b>')  );
-		}
-		<?php endif; ?>
 		function list() {
 			var hashval = window.location.hash.substr(1);
 			$.get('?do=list&file='+ hashval,function(data) {
@@ -590,14 +480,18 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			.attr('href', data.is_dir ? '#' + encodeURIComponent(data.path) : './' + data.path)
 			.text(data.name);
 			var allow_direct_link = <?php echo $allow_direct_link?'true':'false'; ?>;
+
 			if (!data.is_dir && !allow_direct_link)  $link.css('pointer-events','none');
+
 			var $dl_link = $('<a/>').attr('href','?do=download&file='+ encodeURIComponent(data.path))
 			.addClass('download').text('Download');
 			var $delete_link = $('<a href="#" />').attr('data-file',data.path).addClass('delete').text('delete');
 			var perms = [];
+
 			if(data.is_readable) perms.push('read');
 			if(data.is_writable) perms.push('write');
 			if(data.is_executable) perms.push('exec');
+
 			var $html = $('<tr />')
 			.addClass(data.is_dir ? 'is_dir' : '')
 			.append( $('<td class="first" />').append($link) )
@@ -622,6 +516,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			});
 			return $html;
 		}
+
 		function formatTimestamp(unix_timestamp) {
 			var m = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 			var d = new Date(unix_timestamp*1000);
@@ -629,6 +524,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 			(d.getHours() % 12 || 12),":",(d.getMinutes() < 10 ? '0' : '')+d.getMinutes(),
 			" ",d.getHours() >= 12 ? 'PM' : 'AM'].join('');
 		}
+
 		function formatFileSize(bytes) {
 			var s = ['bytes', 'KB','MB','GB','TB','PB','EB'];
 			for(var pos = 0;bytes >= 1000; pos++,bytes /= 1024);
@@ -650,22 +546,7 @@ $MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('uploa
 
 	<body>
 		<div id="top">
-			<?php if($allow_create_folder): ?>
-				<form action="?" method="post" id="mkdir" />
-				<label for=dirname>Create New Folder</label><input id=dirname type=text name=name value="" />
-				<input type="submit" value="create" />
-			</form>
 
-		<?php endif; ?>
-
-		<?php if($allow_upload): ?>
-
-			<div id="file_drop_target">
-				Drag Files Here To Upload
-				<b>or</b>
-				<input type="file" multiple />
-			</div>
-		<?php endif; ?>
 		<div id="breadcrumb">&nbsp;</div>
 	</div>
 
